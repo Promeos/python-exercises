c0110ff6ba58a8044908bb7b94137e45ed275f90
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-08-21 20:02:18 -0500

    Added comments

--- python_introduction_exercises.py
+++ python_introduction_exercises.py
@@ -1,6 +1,13 @@
-# yay Python!
-name = 'World'
+# My first Hello World program using Python
+
+# Assigned string literal "World" to a variable named `name`. Printed the contents 
+# of `name` using f string formatting.
+
+name = "World"
 print(f'Hello {name}!')
 
+# Printing the contents of a variable.
 greeting = "Hello 4th dimensional self!"
 print(greeting)
+
+# Hello again.
