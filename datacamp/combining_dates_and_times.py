af11a654b038b05e915004db25e6916a645c4b2e
promeos <christopher.logan.ortiz@gmail.com>
2021-05-07 12:01:43 -0500

    Uploading datetime exercises from DataCamp

---
+++ datacamp/combining_dates_and_times.py
@@ -0,0 +1,176 @@
+### 1. Creating Datetimes by hand
+
+# Import datetime
+from datetime import datetime
+
+# Create a datetime object
+dt = datetime(2017, 10, 1, 15, 26, 26)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
+
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
+
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Replace the year with 1917
+dt_old = dt.replace(year=1917)
+
+# Print the results in ISO 8601 format
+print(dt_old)
+
+
+### 2. Counting Events Before and After noon
+
+# Create dictionary to hold results
+trip_counts = {'AM': 0, 'PM': 0}
+  
+# Loop over all trips
+for trip in onebike_datetimes:
+  # Check to see if the trip starts before noon
+  if trip['start'].hour < 12:
+    # Increment the counter for before noon
+    trip_counts['AM'] += 1
+  else:
+    # Increment the counter for after noon
+    trip_counts['PM'] += 1
+  
+print(trip_counts)
+
+
+### 3. Turning Strings into Datetimes
+
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in YYYY-MM-DD HH:MM:SS format
+s = '2017-02-03 00:00:01'
+
+# Write a format string to parse s
+fmt = '%Y-%m-%d %H:%M:%S'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
+
+# Starting string, in YYYY-MM-DD format
+s = '2030-10-15'
+
+# Write a format string to parse s
+fmt = '%Y-%m-%d'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
+
+# Starting string, in MM/DD/YYYY HH:MM:SS format
+s = '12/15/1986 08:00:00'
+
+# Write a format string to parse s
+fmt = '%m/%d/%Y %H:%M:%S'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
+
+
+### 4. Parsing Pairs of Strings as Datetimes
+
+# Write down the format string
+fmt = "%Y-%m-%d %H:%M:%S"
+
+# Initialize a list for holding the pairs of datetime objects
+onebike_datetimes = []
+
+# Loop over all trips
+for (start, end) in onebike_datetime_strings:
+  trip = {'start': datetime.strptime(start, fmt),
+          'end': datetime.strptime(end, fmt)}
+  
+  # Append the trip
+  onebike_datetimes.append(trip)
+
+
+### 5. Recreating ISO Format with strftime()
+
+# Import datetime
+from datetime import datetime
+
+# Pull out the start of the first trip
+first_start = onebike_datetimes[0]['start']
+
+# Format to feed to strftime()
+fmt = "%Y-%m-%dT%H:%M:%S"
+
+# Print out date with .isoformat(), then with .strftime() to compare
+print(first_start.isoformat())
+print(first_start.strftime(fmt))
+
+
+### 6. Unix Timestamps
+
+# Import datetime
+from datetime import datetime
+
+# Starting timestamps
+timestamps = [1514665153, 1514664543]
+
+# Datetime objects
+dts = []
+
+# Loop
+for ts in timestamps:
+  dts.append(datetime.fromtimestamp(ts))
+  
+# Print results
+print(dts)
+
+
+### 7. Turning Pairs of Datetimes into Durations
+
+# Initialize a list for all the trip durations
+onebike_durations = []
+
+for trip in onebike_datetimes:
+  # Create a timedelta object corresponding to the length of the trip
+  trip_duration = trip['end'] - trip['start']
+  
+  # Get the total elapsed seconds in trip_duration
+  trip_length_seconds = trip_duration.total_seconds()
+  
+  # Append the results to our list
+  onebike_durations.append(trip_length_seconds)
+
+
+### 8. Average Trip Time
+
+# What was the total duration of all trips?
+total_elapsed_time = sum(onebike_durations)
+
+# What was the total number of trips?
+number_of_trips = len(onebike_durations)
+  
+# Divide the total duration by the number of trips
+print(total_elapsed_time / number_of_trips)
+
+
+### 9. The longest and shortest trip
+
+# Calculate shortest and longest trips
+shortest_trip = min(onebike_durations)
+longest_trip = max(onebike_durations)
+
+# Print out the results
+print("The shortest trip was " + str(shortest_trip) + " seconds")
+print("The longest trip was " + str(longest_trip) + " seconds")
